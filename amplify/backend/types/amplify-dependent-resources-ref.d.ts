aae661a42d2dabfc56c31ea90177314476bc89fe
Hatim aka geoaxis <72706+geoaxis@users.noreply.github.com>
2022-10-30 00:41:04 +0200

    basic amplify api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,22 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "userPoolGroups": {
+            "adminGroupRole": "string"
+        },
+        "amplifycommercedemoac357cb1ac357cb1": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "bookstoreapi": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateBook = /* GraphQL */ `
+  subscription OnCreateBook($filter: ModelSubscriptionBookFilterInput) {
+    onCreateBook(filter: $filter) {
+      id
+      title
+      description
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateBook = /* GraphQL */ `
+  subscription OnUpdateBook($filter: ModelSubscriptionBookFilterInput) {
+    onUpdateBook(filter: $filter) {
+      id
+      title
+      description
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteBook = /* GraphQL */ `
+  subscription OnDeleteBook($filter: ModelSubscriptionBookFilterInput) {
+    onDeleteBook(filter: $filter) {
+      id
+      title
+      description
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
