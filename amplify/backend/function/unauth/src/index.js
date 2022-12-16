ed0b98221e3450231773c4dac42cc1627611c8b5
Hatim aka geoaxis <72706+geoaxis@users.noreply.github.com>
2022-12-16 13:39:58 +0100

    some loggin

--- amplify/backend/function/unauth/src/index.js
+++ amplify/backend/function/unauth/src/index.js
@@ -11,5 +11,6 @@ const server = awsServerlessExpress.createServer(app);
  */
 exports.handler = (event, context) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
+  console.log("Hello");
   return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
 };
