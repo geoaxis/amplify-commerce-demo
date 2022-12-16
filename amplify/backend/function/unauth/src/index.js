03efda57dc304eac1bc103fbd243d8df62c3a995
Hatim aka geoaxis <72706+geoaxis@users.noreply.github.com>
2022-12-16 16:45:36 +0100

    sts

--- amplify/backend/function/unauth/src/index.js
+++ amplify/backend/function/unauth/src/index.js
@@ -1,5 +1,25 @@
+import { , AmazonCognitoIdentity } from 'amazon-cognito-identity-js';
+
 const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./app');
+
+exports.handler = async () => {
+  const { Parameters } = await (new aws.SSM())
+    .getParameters({
+      Names: ["USERNAME","PASSWORD"].map(secretName => process.env[secretName]),
+      WithDecryption: true,
+    })
+    .promise();
+  const USERNAME = Parameters.pop().Value;
+  const PASSWORD = Parameters.pop().Value;
+
+  const response = {
+    statusCode: 200,
+    body: `USERNAME: ${USERNAME}, PASSWORD: ${PASSWORD}`,
+  };
+
+  return response;
+};
 
 /**
  * @type {import('http').Server}
@@ -12,9 +32,43 @@ const server = awsServerlessExpress.createServer(app);
 exports.handler = (event, context) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
 
+  
+
 
   
   console.log(process.env.AUTH_AMPLIFYCOMMERCEDEMOAC357CB1AC357CB1_USERPOOLID);
+  console.log(process.env.USERNAME);
+  console.log(process.env.PASSWORD);
 
-  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
+
+
+  var authenticationData = {
+    Username : process.env.USERNAME,
+    Password : process.env.PASSWORD,
+};
+var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+var poolData = { UserPoolId : process.env.AUTH_AMPLIFYCOMMERCEDEMOAC357CB1AC357CB1_USERPOOLID,
+    ClientId : '1example23456789'
+};
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+var userData = {
+    Username : process.env.USERNAME,
+    Pool : userPool
+};
+var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function (result) {
+        var accessToken = result.getAccessToken().getJwtToken();
+
+        /* Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer */
+        var idToken = result.idToken.jwtToken;
+    },
+
+    onFailure: function(err) {
+        alert(err);
+    },
+
+});
+
+  return cognitoUser;
 };
